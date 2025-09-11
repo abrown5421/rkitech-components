9ca33cc252f3137728a3a95cc24c3cfc270c19ec
abrown5421 <augustashtonbrown@gmail.com>
2025-09-11 18:51:18 -0400

    adding in an input component that is capable of controlling a local or global state using user driven input. Includes animation and tailwind classes

---
+++ src/components/input/inputTypes.ts
@@ -0,0 +1,14 @@
+import { InputHTMLAttributes, ReactNode } from "react";
+import { AnimationObject } from "../../shared/types/animationTypes";
+
+export interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'className'> {
+  animationObject?: AnimationObject;
+  tailwindClasses?: string;
+  label?: string;
+  error?: boolean;
+  helperText?: string;
+  startAdornment?: ReactNode;
+  endAdornment?: ReactNode;
+  multiline?: boolean;
+  rows?: number | 'fill';
+}
