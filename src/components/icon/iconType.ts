9bb52a616d6d90d8e0e341a9aaecd92b2ef4d941
abrown5421 <augustashtonbrown@gmail.com>
2025-09-11 13:57:49 -0400

    adding ina shared type file for tailwind color types and utilizing them in the Icon type declaration

--- src/components/icon/iconType.ts
+++ src/components/icon/iconType.ts
@@ -1,10 +1,12 @@
 import type { AnimationObject } from "../../shared/types/animationTypes";
+import { TailwindColor, TailwindIntensity } from "../../shared/types/tailwindTypes";
 
 export interface IconProps {
   iconName: string; 
   tailwindClasses?: string;
   animationObject?: AnimationObject;
   style?: React.CSSProperties;
-  color?: string; 
+  color?: TailwindColor | 'parent'; 
+  intensity?: TailwindIntensity;
   size?: number | string; 
 }
