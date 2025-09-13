a81c8634523a027e5f3c04b71a861a4bba8845ee
abrown5421 <augustashtonbrown@gmail.com>
2025-09-13 11:14:08 -0400

    adding a checbox component

---
+++ src/components/checkbox/checkboxTypes.ts
@@ -0,0 +1,14 @@
+import { InputHTMLAttributes } from "react";
+import { AnimationObject } from "../../shared/types/animationTypes";
+
+export interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'className' | 'type' | 'size'> {
+  animationObject?: AnimationObject;
+  tailwindClasses?: string;
+  label?: string;
+  error?: boolean;
+  helperText?: string;
+  indeterminate?: boolean;
+  color?: string;
+  intensity?: number;
+  size?: 'sm' | 'md' | 'lg';
+}
