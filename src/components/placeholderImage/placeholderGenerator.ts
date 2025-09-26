1ad9573ce04c1cb328cadb09b8c1c775c9ce47b3
abrown5421 <augustashtonbrown@gmail.com>
2025-09-26 09:26:07 -0400

    modifying the random generator so that it accepts a pool of colors to use rather than hard coding the pool in the generator. This way users of the randomizer can have some control over the pool of colors

--- src/components/placeholderImage/placeholderGenerator.ts
+++ src/components/placeholderImage/placeholderGenerator.ts
@@ -1,29 +1,5 @@
 import { TailwindColorObject } from "../../shared/types/tailwindTypes";
 
-const colorPool: TailwindColorObject[] = [
-  { color: 'amber', intensity: 300 },
-  { color: 'amber', intensity: 400 },
-  { color: 'amber', intensity: 500 },
-  { color: 'amber', intensity: 600 },
-  { color: 'amber', intensity: 700 },
-  { color: 'orange', intensity: 300 },
-  { color: 'orange', intensity: 400 },
-  { color: 'orange', intensity: 500 },
-  { color: 'orange', intensity: 600 },
-  { color: 'orange', intensity: 700 },
-  { color: 'yellow', intensity: 400 },
-  { color: 'yellow', intensity: 500 },
-  { color: 'yellow', intensity: 600 },
-  { color: 'yellow', intensity: 700 },
-  { color: 'yellow', intensity: 800 },
-  { color: 'red', intensity: 500 },
-  { color: 'red', intensity: 600 },
-  { color: 'red', intensity: 700 },
-  { color: 'red', intensity: 800 },
-  { color: 'red', intensity: 900 },
-  { color: 'gray', intensity: 900 },
-];
-
 const getRandomInt = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -36,7 +12,7 @@ const getRandomSubset = (arr: TailwindColorObject[], minSize = 2, maxSize = 5): 
   return shuffled.slice(0, size);
 };
 
-export const placeholderGenerator = () => {
+export const placeholderGenerator = (colorPool: TailwindColorObject[]) => {
   return {
     cellSize: getRandomInt(10, 100),
     variance: parseFloat(getRandomFloat(0.1, 1).toFixed(2)),
